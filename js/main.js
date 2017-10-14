338e5c4ed2c8ed540277d5f85d9eb3ad3ddfd598
francis gallagher <fgallag00@gmail.com>
2017-10-13 20:55:13 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []
+var cardOne = cards[0];
+cardsInPlay.push('queen');
+alert("User flipped Queen");
+var cardTwo = cards[2];
+cardsInPlay.push('king');
+alert("User flipped king");
+if (cardsInPlay === 2){
+	cardsInPlay.length[2];
+} else if (cardsInPlay === 0) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
 
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+
 
 
